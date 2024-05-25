9ca9089143a4e8ace06eca3bb83d546a1f20c722
Sean Hayes <seanowenhayes@yandex.com>
2024-05-25 10:16:13 +0200

    Change to Use pointers, still not fast enough...

--- three-sum.ts
+++ three-sum.ts
@@ -3,44 +3,47 @@ import {
   assertEquals,
 } from "https://deno.land/std@0.224.0/assert/mod.ts";
 
-function combos(arrayLength: number): number[][] {
-  const results: number[][] = [];
-  const used: boolean[] = new Array(arrayLength).fill(false);
-
-  function permute(current: number[], startIndex: number) {
-    if (current.length === 3) {
-      results.push([...current]); // Copy current array to avoid mutation
-      return;
-    }
-
-    for (let i = startIndex; i < arrayLength; i++) {
-      if (!used[i]) {
-        used[i] = true;
-        current.push(i);
-        permute(current, i + 1); // Start from next index to avoid duplicates
-        current.pop();
-        used[i] = false;
-      }
-    }
-  }
-
-  permute([], 0);
-  return results;
+/**
+ * Checks if a given number is present in the sorted array.
+ *
+ * @param {number[]} array - The sorted array to search in.
+ * @param {number} num - The number to search for.
+ * @return {boolean} Returns true if the number is found in the array, false otherwise.
+ */
+function arrayContainsNumber(array: number[], num: number): boolean {
+  // TODO: Maybe do this later as an optimisation using binary search
+  return array.includes(num);
 }
 
 function threeSum(nums: number[]): number[][] {
-  const combinations = combos(nums.length);
-  const resultMap: Map<string, number[]> = new Map();
-  combinations.forEach(([first, second, third]) => {
-    const firstNumber = nums[first];
-    const secondNumber = nums[second];
-    const thirdNumber = nums[third];
-    if (firstNumber + secondNumber + thirdNumber === 0) {
-      const entry = [firstNumber, secondNumber, thirdNumber].sort();
-      resultMap.set(entry.join(), entry);
+  const sortedArray = nums.sort((a, b) => a - b);
+  // This pointer will start at the smalles numbers and move toward the positive larger end
+  let basePointer = 0;
+  // This pointer runs up in front of the base pointer finding numbers to compare
+  let runningPointer = 1;
+  // We only run until the penultimate element.
+  let runningPointerEnd = sortedArray.length - 1;
+
+  const threeSumsMap: Map<string, Array<number>> = new Map();
+
+  while (sortedArray[basePointer] < 1) {
+    const baseNumber = sortedArray[basePointer];
+    for (runningPointer; runningPointer < runningPointerEnd; runningPointer++) {
+      const searchArray = sortedArray.slice(runningPointer + 1);
+      const runningNumber = sortedArray[runningPointer];
+
+      const numberToFind = 0 - (baseNumber + runningNumber);
+      if (arrayContainsNumber(searchArray, numberToFind)) {
+        const toAdd = [baseNumber, runningNumber, numberToFind].sort((a, b) =>
+          a - b
+        );
+        threeSumsMap.set(toAdd.join(), toAdd);
+      }
     }
-  });
-  return Array.from(resultMap.values());
+    basePointer++;
+    runningPointer = basePointer + 1;
+  }
+  return Array.from(threeSumsMap.values());
 }
 
 Deno.test("[-1,0,1,2,-1,-4]", () => {
