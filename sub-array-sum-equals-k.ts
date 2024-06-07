40a5ea2ece239d38348777bc3fc646491ca2cefa
Sean Hayes <seanowenhayes@yandex.com>
2024-06-07 21:34:07 +0200

    Another one done :)

--- sub-array-sum-equals-k.ts
+++ sub-array-sum-equals-k.ts
@@ -3,8 +3,49 @@
 import { assertEquals } from "https://deno.land/std@0.224.0/assert/mod.ts";
 
 function subarraySum(nums: number[], k: number): number {
-  return 99;
+  const numLength = nums.length;
+  let subArrayCount = 0;
+  for (let numsIndex = 0; numsIndex < numLength; numsIndex++) {
+    const num = nums[numsIndex];
+    if (num === k) {
+      subArrayCount += 1;
+    }
+    let scanBackSum = num;
+    for (
+      let scanBackIndex = numsIndex - 1;
+      scanBackIndex >= 0;
+      scanBackIndex--
+    ) {
+      const seenNumber = nums[scanBackIndex];
+      scanBackSum += seenNumber;
+      if (scanBackSum === k) {
+        subArrayCount++;
+      }
+    }
+  }
+  return subArrayCount;
 }
+
+Deno.test("[0,0,0,0,0,0,0,0,0,0]", () => {
+  const input = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+  const k = 0;
+  const output = subarraySum(input, k);
+  assertEquals(output, 55);
+});
+
+Deno.test("[1,-1,0]", () => {
+  const input = [1, -1, 0];
+  const k = 0;
+  const output = subarraySum(input, k);
+  assertEquals(output, 3);
+});
+
+Deno.test("[-1,-1,1]", () => {
+  const input = [-1, -1, 1];
+  const k = 0;
+  const output = subarraySum(input, k);
+  assertEquals(output, 1);
+});
 
 Deno.test("[1,1,1] 2", () => {
   const input = [1, 1, 1];
@@ -15,7 +56,7 @@ Deno.test("[1,1,1] 2", () => {
 
 Deno.test("[1,2,3] => 2", () => {
   const input = [1, 2, 3];
-  const k = 2;
+  const k = 3;
   const output = subarraySum(input, k);
   assertEquals(output, 2);
 });
