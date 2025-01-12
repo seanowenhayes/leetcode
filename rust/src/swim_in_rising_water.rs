5b8b5e117ac6248ba3326b27ac8b03c5f50d6b4a
Sean <seanowenhayes@yandex.com>
2025-01-12 16:20:27 +0100

    Solve swim in rising water using Rust

--- rust/src/swim_in_rising_water.rs
+++ rust/src/swim_in_rising_water.rs
@@ -1,105 +1,112 @@
 use std::collections::HashSet;
 
-struct Route {
-    visited: HashSet<i32>,
-    finished: bool,
-    position: (i32, i32),
-}
+// https://leetcode.com/problems/swim-in-rising-water/description/
 
-impl Route {
-    fn visit(&mut self, square: &i32) -> Option<&Self> {
-        if self.visited.contains(square) {
-            return None;
-        }
-        self.visited.insert(*square);
-        return Some(self);
-    }
-
-    fn cost(&self) -> &i32 {
-        self.visited.iter().max().unwrap()
-    }
-}
-
-struct Swimmer {
+pub struct Swimmer {
     grid: Vec<Vec<i32>>,
-    routes: Vec<Route>,
+    grid_size: usize,
+    visited: HashSet<i32>,
 }
 
 impl Swimmer {
     fn new(grid: Vec<Vec<i32>>) -> Self {
-        let route = Route {
-            visited: {
-                let mut set = HashSet::new();
-                set.insert((0, 0));
-                set
-            },
-            finished: false,
-            position: (0, 0),
-        };
-        Swimmer {
+        let grid_size = grid.len() - 1;
+        let visited: HashSet<i32> = HashSet::new();
+        Self {
             grid,
-            routes: vec![route],
+            grid_size,
+            visited,
         }
     }
 
-    fn position_is_valid(&self, position: &(i32, i32)) -> bool {
+    fn swim(&mut self, position: (usize, usize)) -> i32 {
         let (x, y) = position;
-        let grid_size = self.grid.len() as i32;
-        return *x >= 0 && *x < grid_size && *y >= 0 && *y < grid_size;
-    }
-
-    fn swim(&self) -> &i32 {
-        if self.grid.len() == 1 {
-            return &self.grid[0][0];
+        let current = self.grid[x][y];
+        if x == self.grid_size && y == self.grid_size {
+            return current;
         }
-        if self.routes.iter().all(|route| route.finished) {
-            return self.minimal_route_cost();
+        self.visited.insert(current);
+        // up
+        let mut up_cost: Option<i32> = None;
+        if y > 0 {
+            let up_y = y - 1;
+            let up = self.grid[x][up_y];
+            if !self.visited.contains(&up) {
+                up_cost = Some(self.swim((x, up_y)));
+            }
         }
-        self.routes
-            .iter_mut()
-            .filter(|route| !route.finished)
-            .for_each(|route| {
-                let (x, y) = route.position;
-                [(x, y - 1), (x, y + 1), (x - 1, y), (x + 1, y)]
-                    .iter()
-                    .filter(|position| self.position_is_valid(&position))
-                    .for_each(|position: &(i32, i32)| {
-                        if let Some(a_route) =
-                            route.visit(&self.grid[position.0 as usize][position.1 as usize])
-                        {
-                            let finish_position =
-                                (self.grid.len() as i32 - 1, self.grid.len() as i32 - 1);
-                            let new_route = Route {
-                                visited: route.visited.clone(),
-                                finished: position == &finish_position,
-                                position: *position,
-                            };
-                            self.routes.push(new_route);
-                        }
-                    });
-            });
-        return &999;
+        // right
+        let mut right_cost: Option<i32> = None;
+        if x < self.grid_size {
+            let right_x = x + 1;
+            let right = self.grid[right_x][y];
+            if !self.visited.contains(&right) {
+                right_cost = Some(self.swim((right_x, y)));
+            }
+        }
+        // down
+        let mut down_cost: Option<i32> = None;
+        if y < self.grid_size {
+            let down_y = y + 1;
+            let down = self.grid[x][down_y];
+            if !self.visited.contains(&down) {
+                down_cost = Some(self.swim((x, down_y)));
+            }
+        }
+        // left
+        let mut left_cost: Option<i32> = None;
+        if x > 0 {
+            let left_x = x - 1;
+            let left = self.grid[left_x][y];
+            if !self.visited.contains(&left) {
+                left_cost = Some(self.swim((left_x, y)));
+            }
+        }
+        let max_swim_costs = vec![up_cost, right_cost, down_cost, left_cost]
+            .iter()
+            .filter(|x| x.is_some())
+            .map(|x| x.unwrap())
+            .max();
+        if let Some(max_swim_cost) = max_swim_costs {
+            if max_swim_cost > current {
+                return max_swim_cost;
+            } else {
+                return current;
+            }
+        } else {
+            return current;
+        }
     }
-
-    fn minimal_route_cost(&self) -> &i32 {
-        return self.routes.iter().map(|route| route.cost()).min().unwrap();
+    pub fn swim_in_water(&mut self) -> i32 {
+        return self.swim((0, 0));
     }
-}
-
-fn swim_in_rising_water(input: Vec<Vec<i32>>) -> i32 {
-    let swimmer = Swimmer::new(input);
-
-    return *swimmer.swim();
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::swim_in_rising_water::swim_in_rising_water;
+    use crate::swim_in_rising_water::Swimmer;
 
     #[test]
     fn test_example() {
         let input = vec![vec![0, 2], vec![1, 3]];
-        let output = swim_in_rising_water(input);
+        let mut solution = Swimmer::new(input);
+        let output = solution.swim_in_water();
         assert_eq!(output, 3);
+    }
+
+    #[test]
+    fn test_single_grid_zero() {
+        let input = vec![vec![0]];
+        let mut solution = Swimmer::new(input);
+        let output = solution.swim_in_water();
+        assert_eq!(output, 0);
+    }
+
+    #[test]
+    fn test_single_grid_7() {
+        let input = vec![vec![7]];
+        let mut solution = Swimmer::new(input);
+        let output = solution.swim_in_water();
+        assert_eq!(output, 7);
     }
 }
