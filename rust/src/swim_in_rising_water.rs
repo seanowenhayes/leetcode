d7dbdf405a66cd2ade00b34c11c164fc3da70278
Sean <seanowenhayes@yandex.com>
2025-01-19 10:00:51 +0100

    Add more tests

--- rust/src/swim_in_rising_water.rs
+++ rust/src/swim_in_rising_water.rs
@@ -117,4 +117,44 @@ mod tests {
         let output = swimmer.swim_in_water();
         assert_eq!(output, 16);
     }
+
+    #[test]
+    fn test_example_3() {
+        let input = vec![
+            vec![10, 12, 4, 6],
+            vec![9, 11, 3, 5],
+            vec![1, 7, 13, 8],
+            vec![2, 0, 15, 14],
+        ];
+        let mut swimmer = Swimmer::new(input);
+        let output = swimmer.swim_in_water();
+        assert_eq!(output, 16);
+    }
+
+    #[test]
+    fn test_example_4() {
+        let input = vec![
+            vec![0, 1, 2, 3, 4],
+            vec![24, 23, 22, 21, 5],
+            vec![12, 13, 14, 15, 16],
+            vec![11, 17, 18, 19, 20],
+            vec![10, 9, 8, 7, 6],
+        ];
+        let mut swimmer = Swimmer::new(input);
+        let output = swimmer.swim_in_water();
+        assert_eq!(output, 16);
+    }
+
+    #[test]
+    fn test_example_5() {
+        let input = vec![
+            vec![10, 12, 4, 6],
+            vec![9, 11, 3, 5],
+            vec![1, 7, 13, 8],
+            vec![2, 0, 15, 14],
+        ];
+        let mut swimmer = Swimmer::new(input);
+        let output = swimmer.swim_in_water();
+        assert_eq!(output, 14);
+    }
 }
